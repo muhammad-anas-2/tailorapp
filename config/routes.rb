835fe2f854e19d00603c54901f42db0c817d672e
muhammad-anas-2 <bitf16a537@pucit.edu.pk>
2022-07-04 18:12:42 +0500

    for heroku root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home', to: 'welcome#home'
+  root 'welcome#home'
   get 'welcome/about'
   resources :clients
   resources :users
