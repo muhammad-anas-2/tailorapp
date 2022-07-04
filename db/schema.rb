afd645f82a0aaab7974ad7cacae554a5c0aa793d
muhammad-anas-2 <bitf16a537@pucit.edu.pk>
2022-07-04 14:18:20 +0500

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,31 +15,6 @@ ActiveRecord::Schema.define(version: 20220523101354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "charts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "serial_no"
-    t.string   "phone"
-    t.string   "type_name"
-    t.string   "arm"
-    t.string   "shoulder"
-    t.string   "arm_hole"
-    t.string   "neck_line"
-    t.string   "chest"
-    t.string   "waist"
-    t.string   "skirt"
-    t.string   "shalwar"
-    t.string   "paincha"
-    t.text     "description"
-    t.string   "front"
-    t.string   "side"
-    t.string   "hip"
-    t.string   "collar_ban"
-    t.string   "cuff"
-    t.string   "plate"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "clients", force: :cascade do |t|
     t.string   "name"
     t.string   "serial_no"
