be53c4f3d9e05f30bb727c002a1262f3ee9b52d2
muhammad-anas-2 <bitf16a537@pucit.edu.pk>
2022-07-21 18:16:36 +0500

    Added Modifications in captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
-  config.site_key = '6LexQQshAAAAAGFFVQUWMZunt68sWLSXj-KXT71X'
-  config.secret_key= '6LexQQshAAAAAFLRAxBtjQqqRQEMM2P4Aq63pT11'
+  config.site_key = '6LdgaAshAAAAAD_TSduD7D7y0eJiK3oHRWnnp7Nz'
+  config.secret_key= '6LdgaAshAAAAAB4jOZUu3M2sEHJ1wJOJBH7r8Uy0'
+
 end
